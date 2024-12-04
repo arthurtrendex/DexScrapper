2d07dc79901294fda7451f664345dc3aa7e39a82
Arthur Lambotte <arthur@trendex.tech>
2024-12-04 11:07:17 +0100

    debug screen

--- dexscraper/a.py
+++ dexscraper/a.py
@@ -4,6 +4,7 @@ with SB(uc=True, test=True, locale_code="en") as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.activate_cdp_mode(url)
     sb.uc_gui_click_captcha()
+    sb.save_screenshot("screen.png", folder="./downloaded")
     sb.assert_text("Username", '[for="user_login"]', timeout=3)
     sb.assert_element('label[for="user_login"]')
     sb.highlight('button:contains("Sign in")')
